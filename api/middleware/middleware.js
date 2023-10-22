023468d02f4443ef651d131cb123f60c47a8d5ab
Austin <austin.amaral3@gmail.com>
2023-10-21 18:39:15 -0700

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,20 +1,46 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   const timestamp = new Date().toLocaleString()
   const method = req.method
   const url = req.originalUrl
-  // console.log(`[${timestamp}] ${method} to ${url}`)
+  console.log(`[${timestamp}] ${method} to ${url}`)
   next()
 }
 
-function validateUserId(req, res, next) {
-  next()
+async function validateUserId(req, res, next) {
+  try {
+    const { id } = req.params
+    const user = await User.getById(id)
+    if (!user) {
+      res.status(400).json({
+        message: "user not found",
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "problem finding user",
+    })
+  }
 }
 
 function validateUser(req, res, next) {
-  next()
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: "missing required name field",
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
+  console.log("validatePost middleware")
   next()
 }
 
